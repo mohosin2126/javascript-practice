f68ec082e42d6bffc0a69f20927f59b94fcd25c1
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 21:04:32 +0600

    added another condition

--- multiifelse.js
+++ multiifelse.js
@@ -19,3 +19,20 @@ console.log(payAmount)
 else{
     console.log(price)
 }
+
+
+// another one 
+ const age =17 
+ const foodPrice =500 
+if(age<=12){
+    
+    console.log("you can eat for free")
+}
+else if(age>=60){
+    const discount =price * 50 /100
+    const payAmount =price-discount 
+    console.log(payAmount)
+}
+else{
+    console.log(price)
+}
