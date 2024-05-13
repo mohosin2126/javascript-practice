a0558491dc74af50316d41c3b33340a808363b0a
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-14 00:10:11 +0600

    array done without shift and unshift

---
+++ array.js
@@ -0,0 +1,30 @@
+// array length
+const number=[1,4,53,324,234,3]
+console.log(number.length)
+
+
+// array index
+const index=[1,23,4,34,542,234,4]
+console.log(index[5])
+
+
+// set or update value by index
+const set=[1,23,4,34,542,234,4]
+let update=45
+update=99
+set[1]=999
+console.log(set)
+
+
+// add 
+const ages=[]
+const numbers=[12.23,234,563,356]
+numbers.push(34,356,356,234,23)
+
+
+// remove
+const friends=["balam","kalam","salam","khailam"]
+friends.pop()
+console.log(friends)
+
+
--- logicalnot.js
+++ logicalnot.js
@@ -1,3 +1,5 @@
+// jodi is passed sotto nah hoy tahole code execute korbeh onnothay else e jabe 
+
 const isPassed=false 
 if(!isPassed){
     console.log("toke biya diya dimu")
@@ -5,4 +7,3 @@ if(!isPassed){
 else{
 console.log("tumi amader noyon er moni ")
 }
-// jodi is passed sotto nah hoy tahole code execute korbeh onnothay else e jabe 
