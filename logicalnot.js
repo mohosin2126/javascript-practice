bf5fa4496a045f8e756fd8a972c0e81f6161ac67
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 23:42:34 +0600

    logical not done

---
+++ logicalnot.js
@@ -0,0 +1,8 @@
+const isPassed=false 
+if(!isPassed){
+    console.log("toke biya diya dimu")
+}
+else{
+console.log("tumi amader noyon er moni ")
+}
+// jodi is passed sotto nah hoy tahole code execute korbeh onnothay else e jabe 
