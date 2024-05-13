c769e63aa11a19030c1e16da74c08f11d89c0ad6
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 23:19:47 +0600

    basic ternary done

---
+++ Ternary.js
@@ -0,0 +1,25 @@
+/**
+ * Ternary --->Three Parts 
+ * condition ? do something when true : do something when false 
+ */
+
+
+
+// simple ternary 
+const age=20 
+age>=18 ? console.log("vote dio ") : console.log("ghumai thako")
+
+
+
+let price =500 
+const isLeader =true 
+
+// if(isLeader===true){
+//     price=0 
+// }
+// else{
+//     price=price+100
+// }
+// console.log(price)
+price=isLeader==true?0 : price+100
+console.log(price)
