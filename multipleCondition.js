2cdb70caa5772b0119d0420c38412fda35d31600
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 20:49:59 +0600

    multiple condition basic review done

---
+++ multipleCondition.js
@@ -0,0 +1,20 @@
+const salary =2500 
+const isBCS=true 
+const height =61
+if (salary>2000 && height>66){
+    console.log("super")
+}
+else{
+    console.log("onnoPatrro")
+}
+
+
+
+
+// Another example 
+if(salary>2500 || height>72){
+    console.log("eso baba kobul")
+}
+else{
+    console.log("vag tui moqbul")
+}
