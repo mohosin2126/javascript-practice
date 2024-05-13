f5ee2a539d2e2a353870807f12de26fdaacad52b
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-14 00:33:27 +0600

    string done

---
+++ string.js
@@ -0,0 +1,19 @@
+
+// uppercase and lower case
+
+const school ="millennium scholastic school and college"
+
+console.log(school.toUpperCase())
+
+const subject ="Math"
+const book="math"
+
+if(subject===book){
+    console.log("i'm reading the book eibar ami exam e fathay dimu ")
+}
+else{
+    console.log("huday huday portechi")
+}
+
+
+
