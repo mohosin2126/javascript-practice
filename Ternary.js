659e401111427fb609a028cf2e1da551a6dd1cc9
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 23:34:55 +0600

    ternary done

--- Ternary.js
+++ Ternary.js
@@ -11,6 +11,8 @@ age>=18 ? console.log("vote dio ") : console.log("ghumai thako")
 
 
 
+// complex ternary
+
 let price =500 
 const isLeader =true 
 
@@ -22,4 +24,27 @@ const isLeader =true
 // }
 // console.log(price)
 price=isLeader==true?0 : price+100
+console.log(price)
+
+
+// semiAdvance ternary (optional)
+
+
+// if (isLeader===true){
+//     if(price>1000){
+//         price=price/2
+//     }
+//     else{
+//         price=0
+//     }
+   
+// }
+// else{
+//     price=price+1000
+// }
+
+
+
+price=isLeader==true?price>1000?price/2:0:price+1000
+
 console.log(price)
