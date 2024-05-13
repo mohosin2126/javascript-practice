20797688081a8a8ac45334b2a1440b118a803c6b
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 20:59:57 +0600

    added multiifelse condition

---
+++ multiifelse.js
@@ -0,0 +1,21 @@
+/**
+ * multi level condition in if else
+ */
+
+const price =5000
+if(price>=5000){
+    // 10%discount
+    const discount =price*10/1000
+    const payAmount=price-discount
+    console.log(payAmount)
+}
+else if (price>2500){
+// 5% discount 
+const discount =price*5/100 
+const payAmount=price-discount
+console.log(payAmount)
+}
+
+else{
+    console.log(price)
+}
