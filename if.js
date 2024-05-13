df25cb2e71bf569d46a6cbaf96bc818d8d85af0e
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-13 20:19:42 +0600

    if else done

---
+++ if.js
@@ -0,0 +1,40 @@
+// if you buy food for me 
+// i will come 
+/**
+ * if briyani price less than 300 taka 
+ * i will buy 
+ */
+
+/**
+ * STRUCTURE
+ * if(condition){
+ * it will execute
+ * }
+ */
+
+if(3<10){
+    console.log("i will buy a coffe for you ")
+}
+
+
+var biriyaniPrice=200 
+if (biriyaniPrice<300){
+    console.log("mama give me biriyani")
+}
+
+/**
+ * if (condition){
+ * execute condition if condition above is true 
+ * }
+ * 
+ * else{
+ * execute code if condition is false
+ * }
+ */
+var weight =40 
+if (weight<20){
+    console.log("i will cary it by myself")
+}
+else{
+    console.log("i will rent a rickshaw")
+}
