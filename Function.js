8f34e13fc62042590a7276a060726c07ff04e2f0
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-16 16:17:35 +0600

    conditional return done

--- Function.js
+++ Function.js
@@ -24,3 +24,40 @@ function tenTimes(number){
 const number=10
 const result=tenTimes(number)
 console.log(result)
+
+// example-4 (return[recap])
+function add(price1,price2){
+   const total=price1+price2
+return total
+}
+const bill =add(5,80)
+console.log(bill)
+
+// example-5 
+
+function doMath(num1,num2){
+   const sum = num1+num2
+   const diff =num1+num2 
+   const multiply=sum * diff
+   const result = multiply /2 
+   return result
+
+}
+
+const result2 = doMath(10,5)
+console.log(result2)
+
+
+// example-6 [conditional return]
+
+function isEven(){
+   if(number %2 === 0){
+      return true
+   }
+   else{
+      return false
+   }
+}
+
+console.log(isEven(5))
+console.log(isEven(101))
