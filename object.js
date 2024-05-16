f809c6da3e653c834ebac2f84e01911772ca7c65
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-16 10:06:46 +0600

    value access and update done

--- object.js
+++ object.js
@@ -6,8 +6,19 @@ const person={
     married:true
 }
 
+// --access the property value--
+
 // dot notation 
 console.log(person.name)
 
 // bracket notation 
 console.log(person['age'])
+
+
+// --update the value --
+
+person.salary=3000 
+console.log(person)
+
+person['age']=25 
+console.log(person)
