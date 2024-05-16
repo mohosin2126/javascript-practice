ff804a8894845484d52ff531c675cc045f71cfb2
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-16 15:37:26 +0600

    function basic done

--- Function.js
+++ Function.js
@@ -1,3 +1,4 @@
+//example-1
  function square(number){
 console.log(number)
 const borgo=number*number
@@ -6,9 +7,20 @@ console.log(borgo)
  square(12)
 
 
-
+//example-2
  function add(num1,num2){
     const sum=num1+num2
     console.log(sum)
  }
  add(23,34)
+
+
+//example-3 (return)
+
+function tenTimes(number){
+   const result=number*10
+   return result
+}
+const number=10
+const result=tenTimes(number)
+console.log(result)
