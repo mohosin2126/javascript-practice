c14a88454d174f31126316ae9c63e30c738694f6
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-16 10:37:47 +0600

    nested object done

---
+++ Object/nestedObject.js
@@ -0,0 +1,10 @@
+const college={
+    name:"VNC",
+    class:["11",'12'],
+    events:['science fair','bijoy dibos','21feb'],
+    unique:{
+        color:"blue"
+    }
+}
+
+console.log(college.unique.color)
