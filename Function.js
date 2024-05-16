ee233a3b5f8918eff9ebcff72fd5947bd8a8d303
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-16 15:03:14 +0600

    written a function with parameter

--- Function.js
+++ Function.js
@@ -1,4 +1,14 @@
  function square(number){
 console.log(number)
+const borgo=number*number
+console.log(borgo)
  }
  square(12)
+
+
+
+ function add(num1,num2){
+    const sum=num1+num2
+    console.log(sum)
+ }
+ add(23,34)
