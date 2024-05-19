2c4b14905670da394290eb021a511987acbe4e19
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-19 16:17:05 +0600

    modify the code

--- spreadandrestOperator.js
+++ spreadandrestOperator.js
@@ -3,10 +3,12 @@
  * spread and rest operator *
  */
 
-
+// --spread operator
 const myArray=[1,23,45,53,23]
 const myCopiedArray=[...myArray]
 myArray.push(4)
 myCopiedArray.push(5)
 console.log(myArray)
 console.log(myCopiedArray)
+
+
